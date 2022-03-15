f9834eb33b15bd8e326bcc567b2163b770d183bb
inwells <accounts@straiva.com>
2022-03-15 10:11:42 -0500

    added high score to snake

--- TurtleTime/Snake/score.py
+++ TurtleTime/Snake/score.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 ALIGN = "center"
 FONT = ("Courier", 14, "bold")
+HSFONT = ("Courier", 12, "bold")
+FILE = "data.txt"
 
 class Score(Turtle):
 
@@ -9,6 +11,8 @@ class Score(Turtle):
         self.speed("fastest")
         self.pencolor("white")
         self.score = 0
+        with open(FILE) as f:
+            self.highscore = int(f.read())
         self.ht()
         self.pu()
         self.setposition(0, 270)
@@ -20,6 +24,12 @@ class Score(Turtle):
         self.write(self.score, move=False, align=ALIGN, font=FONT)
 
     def game_over(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open(FILE, mode="w") as f:
+                f.write(str(self.score))
         self.setposition(0,0)
         self.write("GAME OVER", move=False, align=ALIGN, font=FONT)
+        self.setposition(0, -20)
+        self.write(f"High Score: {self.highscore}", align=ALIGN, font=HSFONT)
 
