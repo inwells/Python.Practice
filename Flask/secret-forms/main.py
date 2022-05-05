9ee4b6bd243bfeaa61c589a06f392259461f993d
inwells <accounts@straiva.com>
2022-05-04 23:09:01 -0500

    Bootstrap and blocks add

--- Flask/secret-forms/main.py
+++ Flask/secret-forms/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, regexp, length
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "bbq-Carl"
 
 class LoginForm(FlaskForm):
