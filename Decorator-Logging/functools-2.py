025abf6671178317f1483504d3791f5c9c740a10
inwells <accounts@straiva.com>
2022-06-05 22:48:37 -0500

    Logger wrapper

---
+++ Decorator-Logging/functools-2.py
@@ -0,0 +1,28 @@
+from functools import wraps
+import logging
+
+logging.basicConfig(filename="debug.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logger = logging.getLogger()
+
+def log(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        logger.debug(f"function {func.__name__} called with args {signature}")
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            logger.exception(f"Exception raised in {func.__name__}. exception: {str(e)}")
+            raise e
+    return wrapper
+
+@log
+def foo():
+    raise Exception("Something went wrong")
